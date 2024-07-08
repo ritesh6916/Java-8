2ec64e165cb161a9eb672e89df54c6c820508f07
2100130 <2100130@LTIN578369.cts.com>
2024-07-08 19:28:22 +0530

    created static data & function to print it with predicate.

--- PredicateEmployee/src/main/java/com/cts/PredicateEmployee/App.java
+++ PredicateEmployee/src/main/java/com/cts/PredicateEmployee/App.java
@@ -1,13 +1,40 @@
 package com.cts.PredicateEmployee;
 
+import java.util.ArrayList;
+import java.util.function.Predicate;
+
+import com.cts.PredicateEmployee.templates.Employee;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+	private static ArrayList<Employee> employees = new ArrayList<Employee>();
+	
+	static {
+		employees.add(new Employee("Ritesh","Programmer",30000.0,"kol"));
+		employees.add(new Employee("Ramu","Annalyst",30000.0,"kol"));
+		employees.add(new Employee("Souvik","Manager",30000.0,"kol"));
+		employees.add(new Employee("Shyam","Security",30000.0,"kol"));
+		employees.add(new Employee("Sahanawaj","Manager",30000.0,"kol"));
+	}
+	
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	
+    	
+    }
+    
+    // This method will print elements by checking against the predicate supplied
+    public static void printEmp(Predicate<Employee> pe, ArrayList<Employee> al) {
+    	
+    	for(Employee e : al) {
+    		if(pe.test(e)) {
+    			System.out.println(e);
+    		}
+    	}
+    	
     }
 }
