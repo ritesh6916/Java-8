b0a42683b91d694f44a8f026f0e89da2917e2ed3
2100130 <2100130@LTIN578369.cts.com>
2024-07-08 19:27:22 +0530

    Employee structure added.

--- PredicateEmployee/src/main/java/com/cts/PredicateEmployee/templates/Employee.java
+++ PredicateEmployee/src/main/java/com/cts/PredicateEmployee/templates/Employee.java
@@ -1,5 +1,53 @@
 package com.cts.PredicateEmployee.templates;
 
 public class Employee {
+	
+	private String name;
+	private String designation;
+	private double salary;
+	private String city;
+	
+	public Employee() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Employee(String name, String designation, double salary, String city) {
+		super();
+		this.name = name;
+		this.designation = designation;
+		this.salary = salary;
+		this.city = city;
+	}
 
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDesignation() {
+		return designation;
+	}
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	@Override
+	public String toString() {
+		return "Employee [name=" + name + ", designation=" + designation + ", salary=" + salary + ", city=" + city
+				+ "]";
+	}
+	
 }
