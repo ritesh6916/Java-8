32677ebca58cbc29432a8b832f69857f56a1a99f
2100130 <2100130@LTIN578369.cts.com>
2024-07-10 20:03:05 +0530

    To remove spaces from a String

--- FunctionGen/src/main/java/com/cts/FunctionGen/App.java
+++ FunctionGen/src/main/java/com/cts/FunctionGen/App.java
@@ -1,13 +1,19 @@
 package com.cts.FunctionGen;
 
+import java.util.function.Function;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		String ritesh = "Ritesh Singh is a Java Developer";
+
+		// To remove spaces from a String
+		Function<String, String> removeAllSpace = s -> s.replaceAll(" ", "");
+		System.out.println("Original String: " + ritesh);
+		System.out.println("After removing spaces: " + removeAllSpace.apply(ritesh) + "\n");
+
+	}
 }
