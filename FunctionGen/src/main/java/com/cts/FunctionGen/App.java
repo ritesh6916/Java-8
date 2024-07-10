4157207765ed6e9c0468466c4ea70c32fc398881
2100130 <2100130@LTIN578369.cts.com>
2024-07-10 19:15:35 +0530

    Default code.

---
+++ FunctionGen/src/main/java/com/cts/FunctionGen/App.java
@@ -0,0 +1,13 @@
+package com.cts.FunctionGen;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
