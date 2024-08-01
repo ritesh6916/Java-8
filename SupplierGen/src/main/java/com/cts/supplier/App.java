bb342f704ecf41cf768d07807521caac29caeb32
2100130 <2100130@LTIN578369.cts.com>
2024-08-01 19:20:53 +0530

    Supplier to Select random name

--- SupplierGen/src/main/java/com/cts/supplier/App.java
+++ SupplierGen/src/main/java/com/cts/supplier/App.java
@@ -1,13 +1,24 @@
 package com.cts.supplier;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import java.util.Date;
+import java.util.function.Supplier;
+
+public class App {
+
+	public static void main(String[] args) {
+
+		// Supplier to get System date
+		Supplier<Date> systemDate = () -> new Date();
+		System.out.println("Today's Date & Time: " + systemDate.get());
+
+		// Supplier to Select random name
+		Supplier<String> selectName = () -> {
+
+			String[] names = { "Ritesh", "Shyam", "Anuj", "Rahul", "Beauti", "Prabhat", "Sus" };
+			int index = (int) (Math.random() * 7);
+			return names[index];
+		};
+		System.out.println("Randomly Selected name is: " + selectName.get());
+
+	}
 }
