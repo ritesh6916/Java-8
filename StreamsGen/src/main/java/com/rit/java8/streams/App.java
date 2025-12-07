036667b8221dc5aa3a3f255f04cfa7133be06d87
Ritesh Singh <58039285+ritesh6916@users.noreply.github.com>
2025-12-07 19:50:15 +0530

    Push check.

---
+++ StreamsGen/src/main/java/com/rit/java8/streams/App.java
@@ -0,0 +1,13 @@
+package com.rit.java8.streams;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Streams!" );
+    }
+}
