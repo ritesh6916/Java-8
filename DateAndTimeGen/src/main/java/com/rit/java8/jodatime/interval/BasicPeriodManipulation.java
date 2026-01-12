5bfb0e7b23f59ab7338291305fdff4473d73764b
Ritesh Singh 58039285+ritesh6916@users.noreply.github.com <Ritesh Singh 58039285+ritesh6916@users.noreply.github.com>
2026-01-13 02:35:57 +0530

    calculated time left for the exam

--- DateAndTimeGen/src/main/java/com/rit/java8/jodatime/interval/BasicPeriodManipulation.java
+++ DateAndTimeGen/src/main/java/com/rit/java8/jodatime/interval/BasicPeriodManipulation.java
@@ -1,6 +1,7 @@
 package com.rit.java8.jodatime.interval;
 
 import java.time.LocalDate;
+import java.time.Month;
 import java.time.Period;
 
 public class BasicPeriodManipulation {
@@ -12,7 +13,19 @@ public class BasicPeriodManipulation {
 
 		// Calculating age as of todays date
 		Period p = Period.between(DOB, LocalDate.now());
-		System.out.printf("Your Aget is %d Year %d Month %d Days", p.getYears(), p.getMonths(), p.getDays());
+		System.out.printf("Your Aget is %d Year %d Month %d Days\n", p.getYears(), p.getMonths(), p.getDays());
+
+		// Assume exam Date and create object
+		LocalDate examDate = LocalDate.of(2028, Month.AUGUST, 17);
+
+		// Calculating period of time left
+		Period leftDuration = Period.between(LocalDate.now(), examDate);
+
+		// Calculate days left
+		long totalDays = leftDuration.getYears() * 365 + leftDuration.getMonths() * 30 + leftDuration.getDays();
+
+		System.out.printf("Left time to Prepare is  : %d days %d Months %d Year which is approx %d days ",
+				leftDuration.getDays(), leftDuration.getMonths(), leftDuration.getYears(), totalDays);
 
 	}
 }
